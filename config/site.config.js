6caddf60e092c75a6f1e716468a0a456e7101690
Jim-Zhang1990 <74724952+Jim-Zhang1990@users.noreply.github.com>
2022-07-03 23:16:30 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -3,16 +3,14 @@
 module.exports = {
     global: {
         author: {
-            name: 'Harry Yep',
-            github: 'https://github.com/Harry-Yep/Notion-Blog-React',
-            email: 'hi@harrly.com',
-            privacy_policy: 'https://www.harrly.com/privacy-policy',
+            name: 'Zhen ZHang',
+            github: 'https://github.com/Jim-Zhang1990',
+            email: 'oli@oli-investing.cc',
         },
         site: {
-            name: 'Notion Blog React Example',
-            description: 'Blog built with Notion, React, Next.js, tailwindcss, TypeScript, Notion-Api-Worker and more.',
-            url: 'https://react-notion-blog.demo.harisfox.com/',
-            banner_img: `https://cdn.harrly.com/project/GitHub/Notion-Blog-React/img/Notion-Blog-React.Banner.png`,
+            name: 'Oli-Investing',
+            description: 'Try1..',
+            url: 'https://oli-investing.cc',
             language: [
                 { name: 'British English', code: 'en-GB' },
                 { name: 'Simplified Chinese', code: 'zh-CN' },
@@ -20,12 +18,10 @@ module.exports = {
         },
         content: {
             header: {
-                description: `Blog built with <a href='https://notion.so/' class='transition duration-500 underline hover:bg-yellow-500 dark:hover:bg-yellow-600' target='_blank' rel='noopener noreferrer'>Notion</a>, <a href='https://reactjs.org/' class='transition duration-500 underline hover:bg-yellow-500 dark:hover:bg-yellow-600' target='_blank' rel='noopener noreferrer'>React</a>, <a href='https://nextjs.org/' class='transition duration-500 underline hover:bg-yellow-500 dark:hover:bg-yellow-600' target='_blank' rel='noopener noreferrer'>Next.js</a>, <a href='https://tailwindcss.com/' class='transition duration-500 underline hover:bg-yellow-500 dark:hover:bg-yellow-600' target='_blank' rel='noopener noreferrer'>tailwindcss</a>, <a href='https://www.typescriptlang.org/' class='transition duration-500 underline hover:bg-yellow-500 dark:hover:bg-yellow-600' target='_blank' rel='noopener noreferrer'>TypeScript</a>, <a href='https://github.com/splitbee/notion-api-worker' class='transition duration-500 underline hover:bg-yellow-500 dark:hover:bg-yellow-600' target='_blank' rel='noopener noreferrer'>Notion-Api-Worker</a> and more. (Find more on <a href='https://github.com/Harry-Yep/Notion-Blog-React' class='transition duration-500 underline hover:bg-yellow-500 dark:hover:bg-yellow-600' target='_blank' rel='noopener noreferrer'>GitHub</a>)`,
-                image_url: 'https://cdn.harrly.com/global/assets/icon/android-chrome-192x192.png',
+                description: "Oli's investing notes",
             },
             license: {
-                name: 'CC BY-NC-SA 4.0',
-                url: 'https://creativecommons.org/licenses/by-nc-sa/4.0/',
+        
             },
         },
         analytics: {
